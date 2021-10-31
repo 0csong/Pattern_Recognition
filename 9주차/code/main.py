a0c1a234a96adc0c9ff03a44d38412a7e15caf30
0csong <nickyoungdo@gmail.com>
2021-10-31 22:25:21 +0900

    Update main.py

--- 9주차/code/main.py
+++ 9주차/code/main.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 import time
 #sift와 surf는 opencv 3.4x버전에서만 실행이 가능하다 버전을 낮춰야한다
+#가상환경 설치하고 pip install opencv-contrib-python==3.4.2.16설치
+#설치시 오류나는 경우가 있다. 아나콘다 프롬포트로하니 안되고 파이참 콘솔로하니 된다 경로설정문제?
 
-filepath = 'butterfly.png'
+filepath = 'box.png'
 img = cv2.imread(filepath)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
